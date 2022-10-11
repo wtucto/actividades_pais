3537eb420238e993ecf9363a994dec39ad68a02e
EGARCITI <elvis.gtin@gmail.com>
2022-10-11 12:33:22 -0500

    Avance detalle del proyecto
    Avance detalle del proyecto

--- lib/src/pages/Monitor/main/Project/project_detail_page.dart
+++ lib/src/pages/Monitor/main/Project/project_detail_page.dart
@@ -1,3 +1,4 @@
+import 'package:actividades_pais/backend/model/listar_trama_proyecto_model.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:animated_snack_bar/animated_snack_bar.dart';
@@ -8,28 +9,82 @@ import 'package:get/get.dart';
 const options = ['BAJO', 'MEDIO', 'ALTO'];
 
 class ProjectDetailPage extends StatefulWidget {
-  const ProjectDetailPage({Key? key}) : super(key: key);
+  const ProjectDetailPage({Key? key, required this.datoProyecto})
+      : super(key: key);
+
+  final TramaProyectoModel datoProyecto;
+
   @override
   _ProjectDetailPageState createState() => _ProjectDetailPageState();
 }
 
 class _ProjectDetailPageState extends State<ProjectDetailPage> {
-  final _formKey = GlobalKey<FormState>();
+  late TramaProyectoModel _oProject;
 
-  final _CODIGO_SNIP = TextEditingController(text: "286453");
-  final _CUI = TextEditingController(text: "2191033");
-  final _LONG = TextEditingController(text: "-71.969002");
-  final _LAT = TextEditingController(text: "-14.903419");
-  final _DEPARTAMENTO = TextEditingController(text: "AREQUIPA");
-  final _PROVINCIA = TextEditingController(text: "CASTILLA");
-  final _TAMBO = TextEditingController(text: "CHICOTAÑA");
-  final _CENTRO = TextEditingController(text: "CHICOTANA");
-  final _POBLADO = TextEditingController(text: "CIP_74190");
-  final _COD_RESIDENTE = TextEditingController(text: "RENE RICARDO GONZALES");
-  final _RESIDENTE = TextEditingController(text: "QUISPE");
-  final _COD_SUPERVISOR = TextEditingController(text: "CIP_62352");
-  final _SUPERVISOR = TextEditingController(text: "JUAN VENTURA CASTILLA");
-  final _COD_CRP = TextEditingController(text: "CIP 92822");
+  late final _numSnip;
+  late final _cui;
+  late final _latitud;
+  late final _longitud;
+  late final _departamento;
+  late final _provincia;
+  late final _distrito;
+  late final _tambo;
+  late final _centroPoblado;
+  late final _estado;
+  late final _subEstado;
+  late final _estadoSaneamiento;
+  late final _modalidad;
+  late final _fechaInicio;
+  late final _fechaTerminoEstimado;
+  late final _inversion;
+  late final _costoEjecutado;
+  late final _costoEstimadoFinal;
+  late final _avanceFisico;
+  late final _residente;
+  late final _supervisor;
+  late final _crp;
+  late final _codResidente;
+  late final _codSupervisor;
+  late final _codCrp;
+
+  @override
+  void initState() {
+    _oProject = widget.datoProyecto;
+    _numSnip = TextEditingController(text: _oProject.numSnip);
+    _cui = TextEditingController(text: _oProject.cui);
+    _latitud = TextEditingController(text: _oProject.latitud);
+    _longitud = TextEditingController(text: _oProject.longitud);
+    _departamento = TextEditingController(text: _oProject.departamento);
+    _provincia = TextEditingController(text: _oProject.provincia);
+    _distrito = TextEditingController(text: _oProject.distrito);
+    _tambo = TextEditingController(text: _oProject.tambo);
+    _centroPoblado = TextEditingController(text: _oProject.centroPoblado);
+    _estado = TextEditingController(text: _oProject.estado);
+    _subEstado = TextEditingController(text: _oProject.subEstado);
+    _estadoSaneamiento =
+        TextEditingController(text: _oProject.estadoSaneamiento);
+    _modalidad = TextEditingController(text: _oProject.modalidad);
+    _fechaInicio = TextEditingController(text: _oProject.fechaInicio);
+    _fechaTerminoEstimado =
+        TextEditingController(text: _oProject.fechaTerminoEstimado);
+    _inversion = TextEditingController(text: _oProject.inversion);
+    _costoEjecutado = TextEditingController(text: _oProject.costoEjecutado);
+    _costoEstimadoFinal =
+        TextEditingController(text: _oProject.costoEstimadoFinal);
+    _avanceFisico = TextEditingController(text: _oProject.avanceFisico);
+    _residente = TextEditingController(text: _oProject.residente);
+    _supervisor = TextEditingController(text: _oProject.supervisor);
+    _crp = TextEditingController(text: _oProject.crp);
+    _codResidente = TextEditingController(text: _oProject.codResidente);
+    _codSupervisor = TextEditingController(text: _oProject.codSupervisor);
+    _codCrp = TextEditingController(text: _oProject.codCrp);
+
+    //_POBLADO = TextEditingController(text: _oProject.numSnip);
+
+    super.initState();
+  }
+
+  final _formKey = GlobalKey<FormState>();
 
   final _questionCtrl = TextEditingController();
   final _questionCtrl2 = TextEditingController();
@@ -103,76 +158,195 @@ class _ProjectDetailPageState extends State<ProjectDetailPage> {
         autovalidateMode: AutovalidateMode.onUserInteraction,
         child: ListView(padding: const EdgeInsets.all(32), children: [
           TextFormField(
-            controller: _CODIGO_SNIP,
-            decoration: const InputDecoration(labelText: 'CODIGO SNIP'),
+            controller: _cui,
+            decoration: InputDecoration(labelText: 'FldProyect001'.tr),
             validator: (v) => v!.isEmpty ? 'Required'.tr : null,
             enabled: false,
           ),
           TextFormField(
-              controller: _CUI,
-              decoration: const InputDecoration(labelText: 'CUI'),
-              validator: (v) => v!.isEmpty ? 'Required'.tr : null,
-              enabled: false),
+            controller: _numSnip,
+            decoration: InputDecoration(labelText: 'FldProyect002'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
           TextFormField(
-              controller: _LONG,
-              decoration: const InputDecoration(labelText: 'LONG'),
-              validator: (v) => v!.isEmpty ? 'Required'.tr : null,
-              enabled: false),
+            controller: _latitud,
+            decoration: InputDecoration(labelText: 'FldProyect003'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
           TextFormField(
-              controller: _LAT,
-              decoration: const InputDecoration(labelText: 'LAT'),
-              validator: (v) => v!.isEmpty ? 'Required'.tr : null,
-              enabled: false),
+            controller: _longitud,
+            decoration: InputDecoration(labelText: 'FldProyect004'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
           TextFormField(
-              controller: _DEPARTAMENTO,
-              decoration: const InputDecoration(labelText: 'DEPARTAMENTO'),
-              validator: (v) => v!.isEmpty ? 'Required'.tr : null,
-              enabled: false),
+            controller: _departamento,
+            decoration: InputDecoration(labelText: 'FldProyect005'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
           TextFormField(
-              controller: _PROVINCIA,
-              decoration: const InputDecoration(labelText: 'PROVINCIA'),
-              validator: (v) => v!.isEmpty ? 'Required'.tr : null,
-              enabled: false),
+            controller: _provincia,
+
+            ///NOMBRE DE LA PROVINCIA DEL UBIGEO DEL PROYECTO
+            decoration: InputDecoration(labelText: 'FldProyect006'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
           TextFormField(
-              controller: _TAMBO,
-              decoration: const InputDecoration(labelText: 'TAMBO'),
-              validator: (v) => v!.isEmpty ? 'Required'.tr : null,
-              enabled: false),
+            controller: _distrito,
+
+            ///NOMBRE DEL DISTRITO DEL UBIGEO DEL PROYECTO
+            decoration: InputDecoration(labelText: 'FldProyect007'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
           TextFormField(
-              controller: _CENTRO,
-              decoration: const InputDecoration(labelText: 'CENTRO'),
-              validator: (v) => v!.isEmpty ? 'Required'.tr : null,
-              enabled: false),
+            controller: _tambo,
+
+            ///NOMBRE DEL PROYECTO
+            decoration: InputDecoration(labelText: 'FldProyect008'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
           TextFormField(
-              controller: _POBLADO,
-              decoration: const InputDecoration(labelText: 'POBLADO'),
-              validator: (v) => v!.isEmpty ? 'Required'.tr : null,
-              enabled: false),
+            controller: _centroPoblado,
+
+            ///NOMBRE DEL CENTRO POBLADO DEL UBIGEO DEL PROYECTO
+            decoration: InputDecoration(labelText: 'FldProyect009'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
           TextFormField(
-              controller: _COD_RESIDENTE,
-              decoration: const InputDecoration(labelText: 'COD RESIDENTE'),
-              validator: (v) => v!.isEmpty ? 'Required'.tr : null,
-              enabled: false),
+            controller: _estado,
+
+            ///ESTADO DEL PROYECTO
+            decoration: InputDecoration(labelText: 'FldProyect010'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
           TextFormField(
-              controller: _RESIDENTE,
-              decoration: const InputDecoration(labelText: 'RESIDENTE'),
-              validator: (v) => v!.isEmpty ? 'Required'.tr : null,
-              enabled: false),
+            controller: _subEstado,
+
+            ///SUB ESTADO DEL PROYECTO
+            decoration: InputDecoration(labelText: 'FldProyect011'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
           TextFormField(
-              controller: _COD_SUPERVISOR,
-              decoration: const InputDecoration(labelText: 'COD SUPERVISOR'),
-              validator: (v) => v!.isEmpty ? 'Required'.tr : null,
-              enabled: false),
+            controller: _estadoSaneamiento,
+
+            ///ESTADO DE SANEAMIENTO DEL PROYECTO
+            decoration: InputDecoration(labelText: 'FldProyect012'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
           TextFormField(
-              controller: _SUPERVISOR,
-              decoration: const InputDecoration(labelText: 'SUPERVISOR'),
-              validator: (v) => v!.isEmpty ? 'Required'.tr : null,
-              enabled: false),
+            controller: _modalidad,
+
+            ///MODALIDAD DE CONTRATACIÓN DEL PROYECTO
+            decoration: InputDecoration(labelText: 'FldProyect013'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
           TextFormField(
-              controller: _COD_CRP,
-              decoration: const InputDecoration(labelText: 'COD CRP'),
-              validator: (v) => v!.isEmpty ? 'Required'.tr : null,
-              enabled: false),
+            controller: _fechaInicio,
+
+            ///FECHA DE INICIO DEL PROYECTO
+            decoration: InputDecoration(labelText: 'FldProyect014'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
+          TextFormField(
+            controller: _fechaTerminoEstimado,
+
+            ///FECHA DE TÉRMINO ESTIMADO DEL PROYECTO
+            decoration: InputDecoration(labelText: 'FldProyect015'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
+          TextFormField(
+            controller: _inversion,
+
+            ///MONTO DE INVERSIÓN DEL PROYECTO
+            decoration: InputDecoration(labelText: 'FldProyect016'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
+          TextFormField(
+            controller: _costoEjecutado,
+
+            ///COSTO EJECUTADO ACUMULADO DEL PROYECTO
+            decoration: InputDecoration(labelText: 'FldProyect017'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
+          TextFormField(
+            controller: _costoEstimadoFinal,
+
+            ///COSTO ESTIMADO FINAL DEL PROYECTO
+            decoration: InputDecoration(labelText: 'FldProyect018'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
+          TextFormField(
+            controller: _avanceFisico,
+
+            ///% AVANCE FÍSICO ACUMULADO
+            decoration: InputDecoration(labelText: 'FldProyect019'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
+          TextFormField(
+            controller: _residente,
+
+            ///NOMBRE DEL RESIDENTE
+            decoration: InputDecoration(labelText: 'FldProyect020'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
+          TextFormField(
+            controller: _supervisor,
+
+            ///NOMBRE DEL SUPERVISOR
+            decoration: InputDecoration(labelText: 'FldProyect021'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
+          TextFormField(
+            controller: _crp,
+
+            ///NOMBRE DEL COORDINADOR REGIONAL DEL PROYECTO
+            decoration: InputDecoration(labelText: 'FldProyect022'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
+          TextFormField(
+            controller: _codResidente,
+
+            ///CÓDIGO DEL RESIDENTE
+            decoration: InputDecoration(labelText: 'FldProyect023'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
+          TextFormField(
+            controller: _codSupervisor,
+
+            ///CÓDIGO DEL SUPERVISOR
+            decoration: InputDecoration(labelText: 'FldProyect024'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
+          TextFormField(
+            controller: _codCrp,
+
+            ///CÓDIGO DEL COORDINADOR REGIONAL DEL PROYECTO
+            decoration: InputDecoration(labelText: 'FldProyect025'.tr),
+            validator: (v) => v!.isEmpty ? 'Required'.tr : null,
+            enabled: false,
+          ),
           const SizedBox(height: 32),
         ]),
       ),
