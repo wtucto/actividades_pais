904ebe4ba735feb0d81e77a22e4f80e7def4e20d
Pedro Roner Muñoz Ruiz <roni290690@gmail.com>
2022-11-06 13:16:16 -0500

    Manual de Usuario

--- lib/src/pages/MonitoreoProyectoTambo/main/Settings/main_footer_setting_page.dart
+++ lib/src/pages/MonitoreoProyectoTambo/main/Settings/main_footer_setting_page.dart
@@ -1,9 +1,11 @@
+import 'package:actividades_pais/util/check_connection.dart';
 import 'package:flutter/material.dart';
 
 import 'package:actividades_pais/util/Constants.dart';
 import 'package:actividades_pais/src/pages/MonitoreoProyectoTambo/main/Settings/faq_page.dart';
 import 'package:actividades_pais/src/pages/MonitoreoProyectoTambo/main/Settings/settings_page.dart';
 import 'package:actividades_pais/src/pages/MonitoreoProyectoTambo/main/Settings/legal_about_page.dart';
+import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -25,6 +27,7 @@ class _MainFooterSettingPageState extends State<MainFooterSettingPage> {
 
   String usc = "";
   String usn = "";
+  bool isConnec = false;
 
   loadPreferences() async {
     _prefs = await SharedPreferences.getInstance();
@@ -148,8 +151,98 @@ class _MainFooterSettingPageState extends State<MainFooterSettingPage> {
                       .push(MaterialPageRoute(builder: (_) => FaqPage())),
                 ),
                 const Divider(),
+                ListTile(
+                    title: const Text('Manual de Usuario'),
+                    // subtitle: Text('UserConfig'.tr),
+                    leading: const Icon(
+                      Icons.file_open,
+                      color: Color.fromARGB(221, 104, 101, 101),
+                    ),
+                    trailing: const Icon(
+                      Icons.chevron_right,
+                      color: blue,
+                    ),
+                    onTap: () async {
+                      isConnec = await CheckConnection.isOnlineWifiMobile();
+                      if (isConnec) {
+                        // ignore: use_build_context_synchronously
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute<dynamic>(
+                            builder: (_) => const PDFViewerFromUrl(
+                              url:
+                                  'https://www.pais.gob.pe/sismonitor/FILES/mop_midis.pdf',
+                            ),
+                          ),
+                        );
+                      } else {
+                        // ignore: use_build_context_synchronously
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute<dynamic>(
+                              builder: (_) => const PDFViewerCachedFromUrl(
+                                url:
+                                    'https://www.pais.gob.pe/sismonitor/FILES/mop_midis.pdf',
+                              ),
+                            ));
+                      }
+                    }),
               ],
             ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PDFViewerFromUrl extends StatelessWidget {
+  const PDFViewerFromUrl({Key? key, required this.url}) : super(key: key);
+
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Manual de Usuario'),
+      ),
+      body: const PDF(
+        enableSwipe: true,
+        autoSpacing: false,
+        pageFling: false,
+      ).fromUrl(
+        url,
+        placeholder: (double progress) => Center(child: Text('$progress %')),
+        errorWidget: (dynamic error) => Center(child: Text(error.toString())),
+      ),
+    );
+  }
+}
+
+class PDFViewerCachedFromUrl extends StatelessWidget {
+  const PDFViewerCachedFromUrl({Key? key, required this.url}) : super(key: key);
+
+  final String url;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Manual de Usuario'),
+      ),
+      body: const PDF(
+        enableSwipe: true,
+        autoSpacing: false,
+        pageFling: false,
+      ).cachedFromUrl(
+        url,
+        placeholder: (double progress) => Center(child: Text('$progress %')),
+        errorWidget: (dynamic error) => Center(
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(30, 0, 10, 0),
+            child: const Text(
+                'No se pudo cargar el documento, verifique su conexion a internet'),
           ),
         ),
       ),
