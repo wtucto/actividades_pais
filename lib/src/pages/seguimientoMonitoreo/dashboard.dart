4e79b54046f3e75d2ac6c3d1c19353554eb8fd8e
Pedro Roner Muñoz Ruiz <roni290690@gmail.com>
2022-11-22 00:45:14 -0500

    Incorporando Chart

--- lib/src/pages/seguimientoMonitoreo/dashboard.dart
+++ lib/src/pages/seguimientoMonitoreo/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
@@ -8,11 +9,18 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  // final List<ChartData> chartData = [
+  //   ChartData('David', 25),
+  //   ChartData('Steve', 38),
+  //   ChartData('Jack', 34),
+  //   ChartData('Others', 52)
+  // ];
+  get pieData => null;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("DASHBOARD MONITOR"),
+        title: const Text("Dashboard Monitor"),
         shape: const CustomAppBarShape(multi: 0.05),
         actions: [
           Padding(
@@ -28,9 +36,75 @@ class _DashboardState extends State<Dashboard> {
           )
         ],
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.only(top: 15, left: 15, right: 15),
+          child: Column(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                        color: const Color.fromRGBO(0, 0, 0, 0.12), width: 1.1),
+                    borderRadius: const BorderRadius.all(Radius.circular(12))),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(top: 15, bottom: 2),
+                      child: const Text(
+                        "ESTADO DE TAMBOS",
+                        style: TextStyle(
+                            color: Color.fromRGBO(0, 116, 227, 1),
+                            fontSize: 16,
+                            fontFamily: 'Roboto-Bold'),
+                      ),
+                    ),
+                    const Divider(
+                      color: Color.fromRGBO(61, 61, 61, 1),
+                      // thickness: 1,
+                    ),
+                    Container(
+                      child: SfCircularChart(
+                        // Enables the legend
+                        legend: Legend(
+                            isVisible: true,
+                            position: LegendPosition.bottom,
+                            orientation: LegendItemOrientation.vertical,
+                            overflowMode: LegendItemOverflowMode.wrap),
+                        series: [
+                          DoughnutSeries<ChartData, String>(
+                              dataSource: [
+                                // Bind data source
+                                ChartData('Tambo Culminados', 3),
+                                ChartData('Tambo en Ejecucion', 11),
+                                ChartData('Tambo por Reiniciar', 8),
+                              ],
+                              xValueMapper: (ChartData data, _) => data.x,
+                              yValueMapper: (ChartData data, _) => data.y,
+                              dataLabelSettings: const DataLabelSettings(
+                                  isVisible: true,
+                                  labelAlignment: ChartDataLabelAlignment.top,
+                                  margin: EdgeInsets.all(1)),
+                              name: 'Data')
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
+}
+
+class ChartData {
+  ChartData(this.x, this.y);
+  final String x;
+  final double y;
+  //  Color color;
 }
 
 class CustomAppBarShape extends ContinuousRectangleBorder {
